260942134b75350c66a5afb514dd13bfe67517d1
Sasha Kotlyar <arkotlyar@sep.com>
2018-05-12 19:03:56 -0400

    Refactor HTTP endpoint extensions

--- sama/Extensions/EndpointHttpExtensions.cs
+++ sama/Extensions/EndpointHttpExtensions.cs
@@ -4,6 +4,7 @@ using sama.Models;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq;
 
 namespace sama.Extensions
 {
@@ -11,57 +12,63 @@ namespace sama.Extensions
     {
         private static JsonSerializerSettings JsonSettings = new JsonSerializerSettings { ContractResolver = new DefaultContractResolver() };
 
-        public static string GetHttpLocation(this Endpoint endpoint)
-        {
-            EnsureHttp(endpoint);
-            if (string.IsNullOrWhiteSpace(endpoint.JsonConfig)) return null;
+        public static string GetHttpLocation(this Endpoint endpoint) =>
+            GetValue<string>(endpoint, "Location");
 
-            return JsonConvert.DeserializeObject<dynamic>(endpoint.JsonConfig, JsonSettings).Location?.ToObject<string>();
+        public static void SetHttpLocation(this Endpoint endpoint, string location) =>
+            SetValue(endpoint, "Location", location);
+
+        public static string GetHttpResponseMatch(this Endpoint endpoint) =>
+            GetValue<string>(endpoint, "ResponseMatch");
+
+        public static void SetHttpResponseMatch(this Endpoint endpoint, string responseMatch) =>
+            SetValue(endpoint, "ResponseMatch", responseMatch);
+
+        public static List<int> GetHttpStatusCodes(this Endpoint endpoint) =>
+            GetValueList<int>(endpoint, "StatusCodes");
+
+        public static void SetHttpStatusCodes(this Endpoint endpoint, List<int> codes) =>
+            SetValue(endpoint, "StatusCodes", codes);
+
+
+        private static List<T> GetValueList<T>(Endpoint endpoint, string name, List<T> defaultValue = null)
+        {
+            var list = GetValue<List<object>>(endpoint, name);
+            if (list == null)
+            {
+                return defaultValue;
+            }
+            return list.Select(o => (T)Convert.ChangeType(o, typeof(T))).ToList();
         }
 
-        public static void SetHttpLocation(this Endpoint endpoint, string location)
+        private static T GetValue<T>(Endpoint endpoint, string name, T defaultValue = default(T))
+        {
+            EnsureHttp(endpoint);
+            if (string.IsNullOrWhiteSpace(endpoint.JsonConfig)) return defaultValue;
+
+            var obj = JsonConvert.DeserializeObject<ExpandoObject>(endpoint.JsonConfig, JsonSettings) as IDictionary<string, object>;
+            if (obj == null)
+            {
+                throw new ArgumentException($"Unable to get value for '{name}'");
+            }
+            if (!obj.ContainsKey(name))
+            {
+                return defaultValue;
+            }
+            return (T)obj[name];
+        }
+
+        private static void SetValue<T>(Endpoint endpoint, string name, T value)
         {
             EnsureHttp(endpoint);
 
             var json = (string.IsNullOrWhiteSpace(endpoint.JsonConfig) ? "{}" : endpoint.JsonConfig);
-            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(json, JsonSettings);
-            obj.Location = location;
-            endpoint.JsonConfig = JsonConvert.SerializeObject(obj, JsonSettings);
-        }
-
-        public static string GetHttpResponseMatch(this Endpoint endpoint)
-        {
-            EnsureHttp(endpoint);
-            if (string.IsNullOrWhiteSpace(endpoint.JsonConfig)) return null;
-
-            return JsonConvert.DeserializeObject<dynamic>(endpoint.JsonConfig, JsonSettings).ResponseMatch?.ToObject<string>();
-        }
-
-        public static void SetHttpResponseMatch(this Endpoint endpoint, string responseMatch)
-        {
-            EnsureHttp(endpoint);
-
-            var json = (string.IsNullOrWhiteSpace(endpoint.JsonConfig) ? "{}" : endpoint.JsonConfig);
-            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(json, JsonSettings);
-            obj.ResponseMatch = responseMatch;
-            endpoint.JsonConfig = JsonConvert.SerializeObject(obj, JsonSettings);
-        }
-
-        public static List<int> GetHttpStatusCodes(this Endpoint endpoint)
-        {
-            EnsureHttp(endpoint);
-            if (string.IsNullOrWhiteSpace(endpoint.JsonConfig)) return null;
-
-            return JsonConvert.DeserializeObject<dynamic>(endpoint.JsonConfig, JsonSettings).StatusCodes?.ToObject<List<int>>();
-        }
-
-        public static void SetHttpStatusCodes(this Endpoint endpoint, List<int> codes)
-        {
-            EnsureHttp(endpoint);
-
-            var json = (string.IsNullOrWhiteSpace(endpoint.JsonConfig) ? "{}" : endpoint.JsonConfig);
-            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(json, JsonSettings);
-            obj.StatusCodes = codes;
+            var obj = JsonConvert.DeserializeObject<ExpandoObject>(json, JsonSettings);
+            obj.Remove(name, out object _);
+            if (!obj.TryAdd(name, value))
+            {
+                throw new ArgumentException($"Unable to set value for '{name}'");
+            }
             endpoint.JsonConfig = JsonConvert.SerializeObject(obj, JsonSettings);
         }
 
