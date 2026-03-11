6bce31b1f178ab376602f233ff5069b56e54eeed
Sasha Kotlyar <120513939+arktronic-sep@users.noreply.github.com>
2026-03-11 16:33:58 -0400

    Fix unit tests

--- SAMA.Tests.Unit/Web/Services/OidcPostConfigureOptionsTests.cs
+++ SAMA.Tests.Unit/Web/Services/OidcPostConfigureOptionsTests.cs
@@ -11,37 +11,44 @@ public class OidcPostConfigureOptionsTests
 {
     private GlobalSettingsService _mockGlobalSettings = null!;
     private OidcPostConfigureOptions _postConfigure = null!;
+    private OpenIdConnectOptions _options = null!;
 
     [TestInitialize]
     public void Setup()
     {
         _mockGlobalSettings = Substitute.For<GlobalSettingsService>(null!, null!, null!, null!);
         _postConfigure = new OidcPostConfigureOptions(_mockGlobalSettings);
+        _options = new OpenIdConnectOptions();
+        _options.Backchannel = new HttpClient();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _options.Backchannel.Dispose();
     }
 
     [TestMethod]
     public void PostConfigureShouldIgnoreNonOidcSchemes()
     {
-        var options = new OpenIdConnectOptions();
-        var originalAuthority = options.Authority;
+        var originalAuthority = _options.Authority;
 
-        _postConfigure.PostConfigure("Cookies", options);
+        _postConfigure.PostConfigure("Cookies", _options);
 
-        Assert.AreEqual(originalAuthority, options.Authority);
+        Assert.AreEqual(originalAuthority, _options.Authority);
     }
 
     [TestMethod]
     public void PostConfigureShouldSetDummyConfigWhenDisabled()
     {
         _mockGlobalSettings.OidcEnabled.Returns(false);
-        var options = new OpenIdConnectOptions();
 
-        _postConfigure.PostConfigure(AuthConstants.OidcSource, options);
+        _postConfigure.PostConfigure(AuthConstants.OidcSource, _options);
 
-        Assert.AreEqual("https://localhost", options.Authority);
-        Assert.AreEqual("disabled", options.ClientId);
-        Assert.AreEqual(string.Empty, options.MetadataAddress);
-        Assert.IsNotNull(options.Configuration);
+        Assert.AreEqual("https://localhost", _options.Authority);
+        Assert.AreEqual("disabled", _options.ClientId);
+        Assert.AreEqual(string.Empty, _options.MetadataAddress);
+        Assert.IsNotNull(_options.Configuration);
     }
 
     [TestMethod]
@@ -52,18 +59,17 @@ public class OidcPostConfigureOptionsTests
         _mockGlobalSettings.OidcClientId.Returns("my-client-id");
         _mockGlobalSettings.OidcClientSecret.Returns("my-secret");
         _mockGlobalSettings.OidcScopes.Returns("openid profile email");
-        var options = new OpenIdConnectOptions();
 
-        _postConfigure.PostConfigure(AuthConstants.OidcSource, options);
+        _postConfigure.PostConfigure(AuthConstants.OidcSource, _options);
 
-        Assert.AreEqual("https://login.example.com/tenant", options.Authority);
-        Assert.AreEqual("my-client-id", options.ClientId);
-        Assert.AreEqual("my-secret", options.ClientSecret);
-        Assert.AreEqual(OpenIdConnectResponseType.Code, options.ResponseType);
-        Assert.AreEqual("/signin-oidc", options.CallbackPath.Value);
-        Assert.IsFalse(options.MapInboundClaims);
-        Assert.AreEqual("name", options.TokenValidationParameters.NameClaimType);
-        Assert.AreEqual("my-client-id", options.TokenValidationParameters.ValidAudience);
+        Assert.AreEqual("https://login.example.com/tenant", _options.Authority);
+        Assert.AreEqual("my-client-id", _options.ClientId);
+        Assert.AreEqual("my-secret", _options.ClientSecret);
+        Assert.AreEqual(OpenIdConnectResponseType.Code, _options.ResponseType);
+        Assert.AreEqual("/signin-oidc", _options.CallbackPath.Value);
+        Assert.IsFalse(_options.MapInboundClaims);
+        Assert.AreEqual("name", _options.TokenValidationParameters.NameClaimType);
+        Assert.AreEqual("my-client-id", _options.TokenValidationParameters.ValidAudience);
     }
 
     [TestMethod]
@@ -74,15 +80,14 @@ public class OidcPostConfigureOptionsTests
         _mockGlobalSettings.OidcClientId.Returns("client");
         _mockGlobalSettings.OidcClientSecret.Returns("");
         _mockGlobalSettings.OidcScopes.Returns("openid profile email groups");
-        var options = new OpenIdConnectOptions();
-        options.Scope.Add("pre-existing");
+        _options.Scope.Add("pre-existing");
 
-        _postConfigure.PostConfigure(AuthConstants.OidcSource, options);
+        _postConfigure.PostConfigure(AuthConstants.OidcSource, _options);
 
-        Assert.AreEqual(4, options.Scope.Count);
+        Assert.AreEqual(4, _options.Scope.Count);
         CollectionAssert.AreEquivalent(
             new[] { "openid", "profile", "email", "groups" },
-            options.Scope.ToArray());
+            _options.Scope.ToArray());
     }
 
     [TestMethod]
@@ -93,13 +98,12 @@ public class OidcPostConfigureOptionsTests
         _mockGlobalSettings.OidcClientId.Returns("client");
         _mockGlobalSettings.OidcClientSecret.Returns("");
         _mockGlobalSettings.OidcScopes.Returns("openid  profile   email");
-        var options = new OpenIdConnectOptions();
 
-        _postConfigure.PostConfigure(AuthConstants.OidcSource, options);
+        _postConfigure.PostConfigure(AuthConstants.OidcSource, _options);
 
-        Assert.AreEqual(3, options.Scope.Count);
+        Assert.AreEqual(3, _options.Scope.Count);
         CollectionAssert.AreEquivalent(
             new[] { "openid", "profile", "email" },
-            options.Scope.ToArray());
+            _options.Scope.ToArray());
     }
 }
